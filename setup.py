32c11fed67567550d1b158eacac36b354f3fc8ec
Seth Frost <dovekie@gmail.com>
2017-07-25 21:18:28 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='shortening',
+    packages=['shortening'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
---
+++ shortening/__init__.py
@@ -0,0 +1,1 @@
+from .shortening import app
