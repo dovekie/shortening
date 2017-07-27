3c051a2e6357c059c63dfd5dfb432fa687d6b7ea
Seth Frost <dovekie@gmail.com>
2017-07-27 14:54:53 -0700

    print which config file is used on launch

--- shortening/shortening.py
+++ shortening/shortening.py
@@ -4,7 +4,7 @@ from flask import (Flask, request, session, g, redirect, url_for, abort,
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-print 'Launching with settings: '.format(os.environ['APP_SETTINGS'])
+print 'Launching with settings:', os.environ['APP_SETTINGS']
 
 app.config.update(dict(
     #DATABASE=os.path.join(app.root_path, 'shortening.db'),
